019ad2e02b91eb60fc9e0f9a0bf7cb519e775d91
Vora <darsvora@amazon.com>
2020-10-06 21:41:58 +0530

    Implemented functions tutorial

---
+++ functions.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(5, 10));
+// add(5, '10'); this will throw error
+// optional parameter use ?
+function add1(num1, num2) {
+    return num2 ? (num1 + num2) : num1;
+}
+console.log(add1(5));
+// default parameter
+function add2(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    return num1 + num2;
+}
+console.log(add2(5));
--- functions.ts
+++ functions.ts
@@ -1,2 +1,22 @@
 export {}
 
+function add(num1: number, num2: number): number{
+    return num1 + num2;
+}
+
+console.log(add(5, 10));
+// add(5, '10'); this will throw error
+
+// optional parameter use ?
+function add1(num1: number, num2?: number): number{
+    return num2? (num1 + num2): num1;
+}
+
+console.log(add1(5));
+
+// default parameter
+function add2(num1: number, num2: number = 10): number{
+    return num1 + num2;
+}
+
+console.log(add2(5));
